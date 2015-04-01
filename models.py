050155b190689a6df8a394156d513d42fbfed9ba
mikeyabedneyjr <mike.bedney@gmail.com>
2015-03-31 21:12:52 -0700

    Final push for the night

--- models.py
+++ models.py
@@ -9,43 +9,42 @@ class UserProfile(models.Model):
     #
     # def personal_profile(object):
     # # set picture, later add interests
-    # return
+    #
     # def post_item(object):
     # # Picture, price, description, delivery options (in person/mailed)
-    # return
+    #
     # def messages(object):
     # # actions for sending, receiving, reading messages
-    # return
+    #
     # def shop_page(object):
     # # set banner, biography/artist statement
-    # return
+    #
     # def view_history(object):
     # # Keep track of what user has looked at
-    # return
 
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField(default=0)
-    owner =
+    # TODO: Add association with user
     def __str__(self):
         return self.item_text
 
 
 class Cart(models.Model):
     # def __str__(self):
-    # return self.cart_text
+    #  self.cart_text
     #
     # def add_to_cart(object):
     # # add item logic
-    # return
+    #
     # def remove_from_cart(object):
     # # remove item logic
-    # return
+    #
     # def verify_cart(object):
     # # Logic where users have final opportunity to make cart changes
-    # return
+    #
     # def purchase(object):
     # # Paypal payments go here?
-    # return
+#
