0a6fcff9229162058dd62d05475bdbd0326c5f0b
mikeyabedneyjr <mike.bedney@gmail.com>
2015-04-21 20:22:23 -0700

    Login working

--- buylocalartor/pdxart/urls.py
+++ buylocalartor/pdxart/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, url
 from pdxart import views
 
 urlpatterns = patterns('',
-        url(r'^index/', views.index, name='index'),
+        url(r'^$', views.index, name='index'),
         url(r'^registration/', views.registration, name='registration'),
         url(r'^updateprofile/', views.update_profile, name='updateprofile'),
         url(r'^profile/', views.profile, name='profile'),
+        url(r'^login/$', views.user_login, name='login'),
         )
--- buylocalartor/pdxart/views.py
+++ buylocalartor/pdxart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from pdxart.models import Product, User, UserProfile
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 def index(request):
     # Query the database for a list of ALL products currently stored.
@@ -36,6 +37,47 @@ def registration(request):
         return HttpResponseRedirect('/pdxart/index/')
     return render(request, 'pdxart/registration.html')
 
+
+def user_login(request):
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+                # We use request.POST.get('<variable>') as opposed to request.POST['<variable>'],
+                # because the request.POST.get('<variable>') returns None, if the value does not exist,
+                # while the request.POST['<variable>'] will raise key error exception
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/pdxart/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account has been disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'pdxart/login.html', {})
 
 # Stored profile --------------------------------------------------------------------------------------------------------------------
 @login_required
